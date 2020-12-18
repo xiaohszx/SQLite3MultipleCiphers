eae2d9ef02187bf81f8cec759e0d54462d73495b
utelle <ulrich.telle@gmx.de>
2020-12-18 22:55:27 +0100

    Add test execution

--- test/test1.sql
+++ test/test1.sql
@@ -1,4 +1,4 @@
-.open test1.db3
+.echo on
 pragma cipher='sqlcipher';
 pragma legacy=4;
 pragma key='test1';
@@ -6,6 +6,6 @@ create table t1 (c1 int, c2 char);
 insert into t1 values (1,'Alf');
 insert into t1 values (2,'Bert');
 insert into t1 values (3,'Cecil');
-insert into t1 values (5,'Donald');
+insert into t1 values (4,'Donald');
 select * from t1;
 .q
